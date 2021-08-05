6ee595d451ac1c971fb5bd8a38852fdc695173cb
cavadsalman <cavad2013@gmail.com>
2021-08-05 13:01:45 +0400

    api renderer class

--- elmed/settings.py
+++ elmed/settings.py
@@ -205,4 +205,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 }
